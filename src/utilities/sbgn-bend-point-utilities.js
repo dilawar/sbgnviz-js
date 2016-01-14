bae7c23edba69e35792926388f04936684a85e7f
metincansiper <metincan.55@hotmail.com>
2016-01-14 20:52:39 +0200

    implementing sbgnBendPointUtilities

--- src/utilities/sbgn-bend-point-utilities.js
+++ src/utilities/sbgn-bend-point-utilities.js
@@ -20,12 +20,72 @@ var sbgnBendPointUtilities = {
           node._private.style["border-width"].pxValue / 2);
     }
 
-    return intersect;
+    return {
+      x: intersect[0],
+      y: intersect[1]
+    };
   },
-  convertToRelativeBendPosition: function (edge) {
-    
-  },
-  convertToRealBendPosition: function (edge) {
+  getClippingPointsAndTangents: function (edge) {
+    var sourceNode = edge.source();
+    var targetNode = edge.target();
+    var srcClippingPoint = this.getClippingPoint(sourceNode, targetNode.position('x')
+        , targetNode.position('y'));
+    var tgtClippingPoint = this.getClippingPoint(targetNode, sourceNode.position('x')
+        , sourceNode.position('y'));
 
+    var m1 = (tgtClippingPoint.y - srcClippingPoint.y) / (tgtClippingPoint.x - srcClippingPoint.x);
+    var m2 = -1 / m1;
+
+    return {
+      m1: m1,
+      m2: m2,
+      srcClippingPoint: srcClippingPoint,
+      tgtClippingPoint: tgtClippingPoint
+    };
+  },
+  convertToRelativeBendPosition: function (edge, bendPoint, clippingPointsAndTangents) {
+    if (clippingPointsAndTangents === undefined) {
+      clippingPointsAndTangents = this.getClippingPointsAndTangents(edge);
+    }
+
+    var srcClippingPoint = clippingPointsAndTangents.srcClippingPoint;
+    var tgtClippingPoint = clippingPointsAndTangents.tgtClippingPoint;
+    var m1 = clippingPointsAndTangents.m1;
+    var m2 = clippingPointsAndTangents.m2;
+
+    var a1 = srcClippingPoint.y - m1 * srcClippingPoint.x;
+    var a2 = bendPoint.y - m2 * bendPoint.x;
+
+    var intersectX = (a2 - a1) / (m1 - m2);
+    var intersectY = m1 * srcClippingPoint.x + a1;
+
+    var weight = (intersectX - srcClippingPoint.x) / (tgtClippingPoint.x - srcClippingPoint.x);
+    var distance = Math.sqrt(Math.pow((intersectY - bendPoint.y), 2)
+        + Math.pow((intersectX - bendPoint.x), 2));
+
+    return {
+      weight: weight,
+      distance: distance
+    };
+  },
+  convertToRelativeBendPositions: function (edge) {
+    var clippingPointsAndTangents = this.getClippingPointsAndTangents(edge);
+    var bendPoints = edge.data('bendPointPositions');
+    //output variables
+    var weights = [];
+    var distances = [];
+    
+    for(var i = 0; i < bendPoints.length; i++){
+      var bendPoint = bendPoints[i];
+      var relativeBendPosition = this.convertToRelativeBendPosition(edge, bendPoint, clippingPointsAndTangents);
+      
+      weights.push(relativeBendPosition.weight);
+      distances.push(relativeBendPosition.distance);
+    }
+    
+    return {
+      weights: weights,
+      distances: distances
+    };
   }
 };
