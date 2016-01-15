302a041abe1bb322b6d94f885f68580e91e343da
metincansiper <metincan.55@hotmail.com>
2016-01-16 00:52:47 +0200

    cytoscape.js-noderesize is temporarily removed as it is not competible with the latest version of cytoscape.js

--- sample-app/sampleapp-components/js/sample-app-cytoscape-sbgn.js
+++ sample-app/sampleapp-components/js/sample-app-cytoscape-sbgn.js
@@ -1253,32 +1253,32 @@ var SBGNContainer = Backbone.View.extend({
 
         cy.nodes('[sbgnclass="complex"],[sbgnclass="compartment"],[sbgnclass="submap"]').data('expanded-collapsed', 'expanded');
 
-        cy.noderesize({
-          handleColor: '#000000', // the colour of the handle and the line drawn from it
-          hoverDelay: 1, // time spend over a target node before it is considered a target selection
-          enabled: true, // whether to start the plugin in the enabled state
-          minNodeWidth: 30,
-          minNodeHeight: 30,
-          triangleSize: 10,
-          lines: 3,
-          padding: 5,
-          start: function (sourceNode) {
-            // fired when noderesize interaction starts (drag on handle)
-            var param = {
-              node: sourceNode,
-              firstTime: true
-            };
-            editorActionsManager._do(new ResizeNodeCommand(param));
-            refreshUndoRedoButtonsStatus();
-          },
-          complete: function (sourceNode, targetNodes, addedEntities) {
-            // fired when noderesize is done and entities are added
-          },
-          stop: function (sourceNode) {
-            sourceNode._private.data.sbgnbbox.w = sourceNode.width();
-            sourceNode._private.data.sbgnbbox.h = sourceNode.height();
-          }
-        });
+//        cy.noderesize({
+//          handleColor: '#000000', // the colour of the handle and the line drawn from it
+//          hoverDelay: 1, // time spend over a target node before it is considered a target selection
+//          enabled: true, // whether to start the plugin in the enabled state
+//          minNodeWidth: 30,
+//          minNodeHeight: 30,
+//          triangleSize: 10,
+//          lines: 3,
+//          padding: 5,
+//          start: function (sourceNode) {
+//            // fired when noderesize interaction starts (drag on handle)
+//            var param = {
+//              node: sourceNode,
+//              firstTime: true
+//            };
+//            editorActionsManager._do(new ResizeNodeCommand(param));
+//            refreshUndoRedoButtonsStatus();
+//          },
+//          complete: function (sourceNode, targetNodes, addedEntities) {
+//            // fired when noderesize is done and entities are added
+//          },
+//          stop: function (sourceNode) {
+//            sourceNode._private.data.sbgnbbox.w = sourceNode.width();
+//            sourceNode._private.data.sbgnbbox.h = sourceNode.height();
+//          }
+//        });
 
         //For adding edges interactively
         cy.edgehandles({
